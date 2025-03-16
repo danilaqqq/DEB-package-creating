3d31d370e7cfdb51c226217b4f4783297f7e1a75
Voznesensky Dan <120110797+danilaqqq@users.noreply.github.com>
2025-03-17 02:00:39 +0300

    Update test.sh

--- cicd/test.sh
+++ cicd/test.sh
@@ -3,10 +3,17 @@ set -e
 
 echo "Запуск тестов..."
 
+BINARY_PATH="./build/word_count"
+
+if [[ ! -f "$BINARY_PATH" ]]; then
+    echo "Ошибка: бинарник $BINARY_PATH не найден!"
+    exit 1
+fi
+
 # Тест 1
 input1="Hello world!"
 expected1=2
-output1=$(echo "$input1" | /usr/bin/word_count)
+output1=$(echo "$input1" | "$BINARY_PATH")
 
 if [[ "$output1" -eq "$expected1" ]]; then
     echo "Тест 1 пройден: '$input1' → $output1 слов"
@@ -18,7 +25,7 @@ fi
 # Тест 2
 input2="ya lublu ychitsya, blin klass"
 expected2=5
-output2=$(echo "$input2" | /usr/bin/word_count)
+output2=$(echo "$input2" | "$BINARY_PATH")
 
 if [[ "$output2" -eq "$expected2" ]]; then
     echo "Тест 2 пройден: '$input2' → $output2 слов"
